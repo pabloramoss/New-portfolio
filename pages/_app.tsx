750ff0d500e4bed44396a377e4a0df33b5bb6d75
pablo-pc <pablo-362@hotmail.com>
2022-03-21 13:49:31 -0300

    add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import {ChakraProvider} from "@chakra-ui/react"
+import Script from "next/script"
 import {theme} from "../theme"
 import "../styles/global.css"
 
@@ -11,6 +12,13 @@ function MyApp({ Component, pageProps }) {
       <meta name="Pablo Ramos frontend developer" content="Portfolio Pablo Ramos" />
       <link rel="icon" href="/favicon-32x32.png" />
     </Head>
+    <Script async src="https://www.googletagmanager.com/gtag/js?id=G-R3ERLVF182" />
+    <Script id="google-analytics" strategy="afterInteractive">
+      {`window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-R3ERLVF182');`}
+    </Script>
     <ChakraProvider theme={theme}>
       <Component {...pageProps}  />
     </ChakraProvider>
---
+++ src/AsideBar/MenuItem.tsx
@@ -0,0 +1,16 @@
+import { Divider, Heading, Link } from "@chakra-ui/react";
+import { motion } from "framer-motion";
+import React from "react";
+
+
+const MenuItem: React.FC = () => {
+  const MotionHeading = motion(Heading)
+  
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default MenuItem;
