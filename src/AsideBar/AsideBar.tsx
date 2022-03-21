b6b357292b82e4340ccb8c4f82e81fa5674f5654
pablo-pc <pablo-362@hotmail.com>
2022-03-21 18:07:55 -0300

    fix asidebar code

--- src/AsideBar/AsideBar.tsx
+++ src/AsideBar/AsideBar.tsx
@@ -10,7 +10,10 @@ import {
   useDisclosure, 
   Divider, 
   Text, 
-  DrawerCloseButton
+  DrawerCloseButton,
+  Flex,
+  VStack,
+  Box
 } from '@chakra-ui/react';
 import { FaBars } from 'react-icons/fa';
 import SocialMedia from './SocialMedia';
@@ -20,11 +23,9 @@ function AsideBar() {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const sections = ["home","projects","contact","about me"]
   const MotionHeading = motion(Heading)
-  const menuLinks = sections.map((section) => <Link _hover={{textDecoration: "none"}} key={section} href={`#${encodeURI(section)}`}><MotionHeading whileHover={{ 
-    scale: 1.02}}  _hover={{color:"purple.400"}} fontWeight={400} color="white" fontSize={30} onClick={onClose} my={3} colorScheme="purple">{section.toUpperCase()}</MotionHeading><Divider py={2} /></Link>)
     
   return (
-    <Stack className='asidebar' py={3} bg="none" pos="fixed" w="100%" zIndex={100}>
+    <Stack py={3} bg="none" pos="fixed" w="100%" zIndex={100}>
         <Button 
         position="absolute" 
         right={0} 
@@ -36,12 +37,30 @@ function AsideBar() {
           <FaBars size={30} />
         </Button>
           <Drawer placement="right" onClose={onClose} isOpen={isOpen} size="md">
-            <DrawerContent bg="none" sx={{ backdropFilter: "blur(16px) brightness(40%)"}}>
+            <DrawerContent maxH="100vh" bg="none" sx={{ backdropFilter: "blur(16px) brightness(40%)"}}>
             <DrawerCloseButton color="white" size="2xl" h={10} w={10} />
               <DrawerBody>
                 <Stack pt={{base:10, md:20}} justifyContent="space-between" height="100%">
                   <Stack spacing={8}>
-                    {menuLinks}
+                    {sections.map((section) => 
+                    <Link 
+                    _hover={{textDecoration: "none"}} 
+                    onClick={onClose} 
+                    key={section} 
+                    href={`/#${encodeURI(section)}`}
+                    >
+                      <MotionHeading 
+                      whileHover={{scale: 1.02}}  
+                      _hover={{color:"purple.400"}} 
+                      fontWeight={400} 
+                      color="white" 
+                      fontSize={30} 
+                      my={3} 
+                      colorScheme="purple"
+                      >{section.toUpperCase()}
+                      </MotionHeading>
+                      <Divider />
+                    </Link>)}
                   </Stack>
                   <Stack pb={{base: 5, md: 0}} spacing={5}>
                     <SocialMedia color="gray.400"/>
@@ -51,10 +70,7 @@ function AsideBar() {
                     alignSelf="center" 
                     color="gray.400"
                     >
-                      ©
-                      {new Date().getFullYear()}
-                      {' '}
-                      Pablo Ramos
+                      ©{new Date().getFullYear()}{' '}Pablo Ramos
                     </Text>
                   </Stack>
                 </Stack>
