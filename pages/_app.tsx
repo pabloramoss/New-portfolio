4c3b4f5f8be52fe0e4efac8cb05f9168b2223cb0
pablo-pc <pablo-362@hotmail.com>
2022-04-13 21:41:21 -0300

    add enviroment variable google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,12 +12,12 @@ function MyApp({ Component, pageProps }) {
       <meta name="Pablo Ramos frontend developer" content="Portfolio Pablo Ramos" />
       <link rel="icon" href="/favicon-32x32.png" />
     </Head>
-    <Script async src="https://www.googletagmanager.com/gtag/js?id=G-R3ERLVF182" />
+    <Script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYITCS}`} />
     <Script id="google-analytics" strategy="afterInteractive">
       {`window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
-        gtag('config', 'G-R3ERLVF182');`}
+        gtag('config', ${process.env.GOOGLE_ANALYITCS});`}
     </Script>
     <ChakraProvider theme={theme}>
       <Component {...pageProps}  />
