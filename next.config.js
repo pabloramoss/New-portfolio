6fe6c671a23c3843391cb51ce3efc79ceb18c2e2
pablo-pc <pablo-362@hotmail.com>
2022-05-13 17:37:41 -0300

    add redirect

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/twitter',
+        destination: 'https://twitter.com/rpablo_dev',
+        permanent: true,
+      },
+      {
+        source: '/linkedin',
+        destination: 'https://www.linkedin.com/in/pabloramoss',
+        permanent: true,
+      },
+    ]
+  },
   
 }
 
